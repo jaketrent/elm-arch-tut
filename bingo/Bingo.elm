e4bb7905f9bf254026d63f3258946696c749184a
jaketrent <trent.jake@gmail.com>
2015-08-23 14:37:49 -0600

    Add new phrase to list

--- bingo/Bingo.elm
+++ bingo/Bingo.elm
@@ -7,6 +7,8 @@ import Signal exposing (Address)
 import StartApp.Simple as StartApp
 import String exposing (toUpper, repeat, trimRight)
 
+import BingoUtils as Utils
+
 type alias Entry = {
   phrase: String,
   points: Int,
@@ -15,7 +17,10 @@ type alias Entry = {
 }
 
 type alias Model = {
-  entries: List Entry
+  entries: List Entry,
+  phraseInput: String,
+  pointsInput: String,
+  nextID: Int
 }
 
 type Action
@@ -23,15 +28,21 @@ type Action
   | Sort
   | Delete Int
   | Mark Int
+  | UpdatePhraseInput String
+  | UpdatePointsInput String
+  | Add
 
 initialModel : Model
 initialModel =
   {
-    entries =
-      [ newEntry "Doing Agile" 200 2,
-        newEntry "In the Cloud" 300 3,
-        newEntry "Future Proof" 100 1
-      ]
+    entries = [
+      newEntry "Doing Agile" 200 2,
+      newEntry "In the Cloud" 300 3,
+      newEntry "Future Proof" 100 1
+    ],
+    phraseInput = "",
+    pointsInput = "",
+    nextID = 4
   }
 
 newEntry : String -> Int -> Int -> Entry
@@ -42,6 +53,14 @@ newEntry phrase points id =
     wasSpoken = False,
     id = id
   }
+
+totalPoints : List Entry -> Int
+totalPoints entries =
+  let
+    spokenEntries = List.filter .wasSpoken entries
+  in
+    List.sum (List.map .points spokenEntries)
+
 
 update : Action -> Model -> Model
 update action model =
@@ -59,6 +78,24 @@ update action model =
           if e.id == id then { e | wasSpoken <- (not e.wasSpoken) } else e
       in
         { model | entries <- List.map updateEntry model.entries }
+    UpdatePhraseInput contents -> { model | phraseInput <- contents }
+    UpdatePointsInput contents -> { model | pointsInput <- contents }
+    Add ->
+      let
+        entryToAdd =
+          newEntry model.phraseInput (Utils.parseInt model.pointsInput) model.nextID
+        isInvalid model =
+          String.isEmpty model.phraseInput || String.isEmpty model.pointsInput
+      in
+        if isInvalid model
+        then model
+        else {
+          model |
+            phraseInput <- "",
+            pointsInput <- "",
+            entries <- entryToAdd :: model.entries,
+            nextID <- model.nextID + 1
+        }
 
 title : String -> Int -> Html
 title message times =
@@ -72,19 +109,40 @@ pageHeader : Html
 pageHeader =
   h1 [] [ title "bingo!" 3 ]
 
-totalPoints : List Entry -> Int
-totalPoints entries =
-  let
-    spokenEntries = List.filter .wasSpoken entries
-  in
-    List.sum (List.map .points spokenEntries)
-
 totalItem : Int -> Html
 totalItem total =
   li [ class "total" ] [
     span [ class "label" ] [ text "Total" ],
     span [ class "points" ] [ text (toString total) ]
   ]
+
+entryForm : Address Action -> Model -> Html
+entryForm address model =
+  div [] [
+    input [
+      type' "text",
+      placeholder "Phrase",
+      value model.phraseInput,
+      name "phrase",
+      autofocus True,
+      Utils.onInput address UpdatePhraseInput
+    ] [],
+    input [
+      type' "number",
+      placeholder "Points",
+      value model.pointsInput,
+      name "points",
+      Utils.onInput address UpdatePointsInput
+    ] [],
+    button [
+      class "add",
+      onClick address Add
+    ] [
+      text "Add"
+    ],
+    h2 [] [ text (model.phraseInput ++ " " ++ model.pointsInput) ]
+  ]
+
 
 entryItem : Address Action -> Entry -> Html
 entryItem address entry =
@@ -110,6 +168,7 @@ view : Address Action -> Model -> Html
 view address model =
   div [ id "container" ] [
     pageHeader,
+    entryForm address model,
     entryList address model.entries,
     pageFooter
   ]
